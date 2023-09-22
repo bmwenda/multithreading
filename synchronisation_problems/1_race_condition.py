56b48d248d3fae919a9c543d06c183018f67cdb5
Boniface Mwenda <bonny.mwenda04@gmail.com>
2023-09-22 17:19:52 +0300

    Increase sleep time

--- synchronisation_problems/1_race_condition.py
+++ synchronisation_problems/1_race_condition.py
@@ -13,14 +13,16 @@ class Account:
     def deposit(self):
         for i in range(100000000): # high value to increase the chances of getting race conditions
             self.balance += 1000
+        print("Done depositing")
 
     def withdraw(self):
         for i in range(100000000):
             self.balance -= 1000
+        print("Done withdrawing")
 
 if __name__ == "__main__":
     account = Account()
     Thread(target=account.deposit, args=()).start()
     Thread(target=account.withdraw, args=()).start()
-    time.sleep(5) # allow threads to finish before reading balance
+    time.sleep(10) # allow threads to finish before reading balance
     print("Current balance: %s " % account.balance) # should be 1000
